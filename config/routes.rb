38616a72f1d6d71132e76a0893f749b5bf617bd2
YC <lyc950421@gmail.com>
2016-02-24 04:24:48 +0800

    Migrated Seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   resources :categories
   resources :posts
   root 'homes#index'
+  ActiveAdmin.routes(self)
 end
