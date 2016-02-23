2e6201b69a46ae9d68ec42a8b314eb5e5a226315
YC <lyc950421@gmail.com>
2016-02-23 21:13:46 +0800

    Added new & create action for posts AND updated css style for flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  add_flash_types :error, :alert
   protect_from_forgery with: :exception
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,19 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new 
+    @category = Category.all
   end
 
   def create
+    # ActiveModel::ForbiddenAttributesError(http://api.rubyonrails.org/classes/ActiveModel/ForbiddenAttributesError.html)
+    params.permit!
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path, :notice => "You post have been created!"
+    else
+      render "new", :error => "Invalid Input, Please try again!"
+    end
   end
 
   def edit
