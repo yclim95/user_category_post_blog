d36b0d59c0a8f80ed5dbc81ba2c6719e45587d6c
YC <lyc950421@gmail.com>
2016-02-23 19:58:08 +0800

    Added show action for posts controller, added index & show action

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -6,6 +6,11 @@ class PostsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get show" do
+    get :show
+    assert_response :success
+  end
+
   test "should get new" do
     get :new
     assert_response :success
