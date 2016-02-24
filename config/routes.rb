e845295699c06f45bf4ca389b006d275b5e13b42
YC <lyc950421@gmail.com>
2016-02-24 20:32:24 +0800

    Added static pages Pages & updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+  get 'pages/contact'
+  get 'pages/resources'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   resources :categories
   resources :posts
+
+  get '/about', :to => 'pages#about'
+  get '/contact', :to => 'pages#contact'
+  get '/resources', :to => 'pages#resources'
+  get ':not_found', to: redirect('/') 
   root 'homes#index'
   ActiveAdmin.routes(self)
 end
